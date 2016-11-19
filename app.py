dade47d7bda9e2aab2048e657ba92c6eb873602c
Ryan Chakmak <rchakmak@gmail.com>
2016-11-18 16:40:39 -0800

    Main program completed, implemented add and kick features

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ file = open(credential.csvFile, 'r')
 slack = Slacker(credential.code)
 memberList = slack.users.list()
 
-print memberList.body	
-
+# Declare lists for later
+addList = []
+kickList = []
 
 ## Goes through each line of the database
 for line in file:
@@ -24,16 +25,50 @@ for line in file:
 	## the entire last name. This will help us identify the Slack users. This should work for most names. 
 	emailCode = namePartition[0][0].lower() + namePartition[1].lower()
 
+	# Saves the names of all people wh should be in the group
+	if (dataPartition[2] == 'Y'):
+		addList.append(emailCode)
+		
+	# Saves the names of all the people who shouldn't be in the group
+	else:
+		kickList.append(emailCode)		
+
+memberList = slack.channels.info(credential.generalChannelID)
+userCodesFirstSlice = memberList.raw.split("members\":[", 1)
+userCodesSecondSlice = userCodesFirstSlice[1].split("]", 1)
+userCodes = userCodesSecondSlice[0].split(',')
+
+## Fetches slack userID codes and places them in a list
+idList = []
+for senatorID in userCodes:
+	idList.append(senatorID.replace("\"",""))
 
 
-	#if (dataPartition[2] == 'Y'):
-		#memberList = slack.channels.info(credential.generalChannelID)
-		#Slack API Code (Add to group) 
-	#else:
-		#senatorList = slack.groups.info(credential.senatorChannelID)
-		#Slack API Code (Remove from group)
+for senatorID in idList:
+	
+	## This is horrendous and I will fix it
+	userInfo = slack.users.info(senatorID)
+	userInfoFirstSlice = userInfo.raw.split("\"email\":\"",1)
+	userInfoSecondSlice = userInfoFirstSlice[1].split("@",1)
+	userID1 = userInfoSecondSlice[0].replace("1","")
+	userID2 = userID1.replace("2","")
+	userID3 = userID2.replace("0","")
+	userID4 = userID3.replace("9","")
+	userID5 = userID4.replace("8", "")
+	userID6 = userID5.replace("7","")
+
+	## if the email is in addList, then they are added to the group
+	## if the email is in kickList, then they are kicked from the group
+	## We pass over errors here to avoid the program crashing if someone 
+	## is already not in the group when the program tries to kick them.
+	try: 
+		if userID6 in addList:
+			print userID6 + ' added!'
+			slack.groups.invite(credential.senatorChannelID, senatorID)
+		if userID6 in kickList:
+			print userID6 + ' removed!'
+			slack.groups.kick(credential.senatorChannelID, senatorID)
+	except:
+		pass
 
 file.close()
-
-
-#https://api.slack.com/methods/groups.list/test
